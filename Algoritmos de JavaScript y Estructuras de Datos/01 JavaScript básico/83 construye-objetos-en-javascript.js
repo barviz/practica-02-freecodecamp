62357b07584cb563f85a3195d4e3b37a1b3b537d
Bárbara Vizgarra <96797843+barviz@users.noreply.github.com>
2022-12-30 00:04:23 -0300

    unidad 01 casi finalizada

---
+++ Algoritmos de JavaScript y Estructuras de Datos/01 JavaScript básico/83 construye-objetos-en-javascript.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Cambia solo el código debajo de esta línea
+  "name": "Perro",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Hueso", "Pelota"]
+
+  // Cambia solo el código encima de esta línea
+};
---
+++ Algoritmos de JavaScript y Estructuras de Datos/01 JavaScript básico/96 itera-con-el-bucle-while-de-javascript.js
@@ -0,0 +1,10 @@
+// Configuración
+const myArray = [];
+
+// Cambia solo el código debajo de esta línea
+let i = 5;
+
+while (i > -1) {
+  myArray.push(i);
+  i--;
+}
